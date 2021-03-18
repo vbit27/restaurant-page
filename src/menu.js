9e492a08b88d442ead5ccfca754ba53f31cec41d
Vasil Bit <vasil.bituni@gmail.com>
2021-03-18 12:02:03 +0100

    Add update contact page style

--- src/menu.js
+++ src/menu.js
@@ -1,42 +1,38 @@
 export const appendMenu = () => {
+    const gridContainer = document.createElement('div');
+    gridContainer.classList = 'grid-container';  
     
-const menu = [
-    {img: '../img/tea1.jpg',
-    name: 'Green Tea'},
-    {img: '../img/tea2.jpg',
-    name: 'Black Tea'},
-    {img: '../img/tea3.jpg',
-    name: 'Mint Tea'}
-];
+    const menu = [
+        {img: '../img/tea1.jpg',
+        name: 'Green Tea'},
+        {img: '../img/tea2.jpg',
+        name: 'Black Tea'},
+        {img: '../img/tea3.jpg',
+        name: 'Mint Tea'}
+    ];
+
+    menu.forEach(x => render(x));
 
 
-const gridContainer = document.createElement('div');
-gridContainer.classList = 'grid-container';
+    function render(x) {
+        const img = document.createElement('img');
+        const name = document.createElement('h3');
+        const productCell = document.createElement('div');
 
+        img.setAttribute('src', x.img);
+        name.innerHTML = x.name;
+        productCell.classList = 'cell';
 
-menu.forEach(x => render(x));
+        appendProduct(img, name, productCell);
+    }
+    
 
-function render(x) {
-    const img = document.createElement('img');
-    const name = document.createElement('h3');
-    const productCell = document.createElement('div');
+    function appendProduct(img, name, cell) {
+        cell.appendChild(img);
+        cell.appendChild(name);
+        gridContainer.appendChild(cell);
+    }
 
-    img.setAttribute('src', x.img);
-    name.innerHTML = x.name;
-    productCell.classList = 'cell';
-
-   appendProduct(img, name, productCell);
-}
-
-
-function appendProduct(img, name, cell) {
-    cell.appendChild(img);
-    cell.appendChild(name);
-    gridContainer.appendChild(cell);
-}
-
-return gridContainer;
-
-
+    return gridContainer;
 
 }
